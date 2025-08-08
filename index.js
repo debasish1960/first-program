621f09af48417f294ac9b6407d85a860ee80476f
Debasish Karmokar <124527077+debasish1960@users.noreply.github.com>
2025-08-09 01:10:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello World");
- 
+// Write a program to find the position of a Pattern in a String...
+
+let Name = "Debasish Karmokar";
+console.log("Hello,",Name,"!");
+console.log("Type of the data is:",typeof Name);
+console.log("What is the Position Of K:",Name.indexOf("K"));
+
+console.log("Your name has",Name.length, "Characters");
+console.log("In UpperCase:",Name.toUpperCase());
+console.log("In LowerCase:", Name.toLowerCase());
